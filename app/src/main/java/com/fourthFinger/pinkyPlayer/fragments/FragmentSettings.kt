83f826f6af9caea6035427dd21ed172984492a0d
alexanderjohnston <aljohnston112@gmail.com>
2021-09-11 12:05:52 -0500

    FragmentSettings.kt refactoring. FragmentSettings.kt now has ViewModelSettings.kt validate user input. Removed another unnecessary MediaData dependency.

--- app/src/main/java/com/fourthFinger/pinkyPlayer/fragments/FragmentSettings.kt
+++ app/src/main/java/com/fourthFinger/pinkyPlayer/fragments/FragmentSettings.kt
@@ -7,15 +7,10 @@ import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.fourthFinger.pinkyPlayer.R
-import com.fourthFinger.pinkyPlayer.ToastUtil
-import com.fourthFinger.pinkyPlayer.activity_main.ActivityMain
 import com.fourthFinger.pinkyPlayer.activity_main.ViewModelActivityMain
 import com.fourthFinger.pinkyPlayer.databinding.FragmentSettingsBinding
-import com.fourthFinger.pinkyPlayer.media_controller.MediaData
-import com.fourthFinger.pinkyPlayer.media_controller.SaveFile
 
 class FragmentSettings : Fragment() {
 
@@ -25,22 +20,12 @@ class FragmentSettings : Fragment() {
     private val viewModelActivityMain by activityViewModels<ViewModelActivityMain>()
     private val viewModelSettings by activityViewModels<ViewModelSettings>()
 
-    private lateinit var mediaData: MediaData
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        mediaData = MediaData.getInstance()
-
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.getRoot()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        return binding.root
     }
 
     override fun onResume() {
@@ -53,87 +38,19 @@ class FragmentSettings : Fragment() {
     private fun updateFAB() {
         viewModelActivityMain.setFabImage(R.drawable.ic_check_black_24dp)
         viewModelActivityMain.setFABText(R.string.fab_save)
-        viewModelActivityMain.setFabOnClickListener{
-            // TODO validate in ViewModel
-            val nSongs = getNSongs()
-            if (nSongs == -1) {
-                return@setFabOnClickListener
-            }
-            val percentChangeUp = getPercentChangeUp()
-            if (percentChangeUp == -1) {
-                return@setFabOnClickListener
-            }
-            val percentChangeDown = getPercentChangeDown()
-            if (percentChangeDown == -1) {
-                return@setFabOnClickListener
-            }
-            updateSettings(nSongs, percentChangeUp, percentChangeDown)
-            val navController: NavController = NavHostFragment.findNavController(this)
-            if (navController.currentDestination?.id == R.id.FragmentSettings) {
-                navController.popBackStack()
-            }
+        viewModelActivityMain.setFabOnClickListener {
+            val editTextNSongs: EditText = binding.editTextNSongs
+            val editTextPercentChangeUp: EditText = binding.editTextPercentChangeUp
+            val editTextPercentChangeDown: EditText = binding.editTextPercentChangeDown
+            viewModelSettings.fabClicked(
+                requireActivity().applicationContext,
+                NavHostFragment.findNavController(this),
+                editTextNSongs.text.toString().toInt(),
+                editTextPercentChangeUp.text.toString().toInt(),
+                editTextPercentChangeDown.text.toString().toInt()
+            )
         }
         viewModelActivityMain.showFab(true)
-    }
-
-    private fun getNSongs(): Int {
-        val editTextNSongs: EditText = binding.editTextNSongs
-        var nSongs = -1
-        try {
-            nSongs = editTextNSongs.text.toString().toInt()
-        } catch (e: NumberFormatException) {
-            e.printStackTrace()
-        }
-        if (nSongs < 1) {
-            ToastUtil.showToast(
-                requireActivity().applicationContext,
-                R.string.max_percent_error
-            )
-            nSongs = -1
-        }
-        return nSongs
-    }
-
-    private fun getPercentChangeUp(): Int {
-        val editTextPercentChangeUp: EditText = binding.editTextPercentChangeUp
-        var percentChangeUp = -1
-        try {
-            percentChangeUp = editTextPercentChangeUp.text.toString().toInt()
-        } catch (e: NumberFormatException) {
-            e.printStackTrace()
-        }
-        if (percentChangeUp < 1 || percentChangeUp > 100) {
-            ToastUtil.showToast(
-                requireActivity().applicationContext,
-                R.string.percent_change_error
-            )
-            percentChangeUp = -1
-        }
-        return percentChangeUp
-    }
-
-    private fun getPercentChangeDown(): Int {
-        val editTextPercentChangeDown: EditText = binding.editTextPercentChangeDown
-        var percentChangeDown = -1
-        try {
-            percentChangeDown = editTextPercentChangeDown.text.toString().toInt()
-        } catch (e: NumberFormatException) {
-            e.printStackTrace()
-        }
-        if (percentChangeDown < 1 || percentChangeDown > 100) {
-            ToastUtil.showToast(
-                requireActivity().applicationContext,
-                R.string.percent_change_error
-            )
-            percentChangeDown = -1
-        }
-        return percentChangeDown
-    }
-
-    private fun updateSettings(nSongs: Int, percentChangeUp: Int, percentChangeDown: Int) {
-        val activityMain: ActivityMain = requireActivity() as ActivityMain
-        viewModelSettings.setSettings(nSongs, percentChangeDown, percentChangeUp)
-        SaveFile.saveFile(requireActivity().applicationContext)
     }
 
     private fun loadSettings() {
--- app/src/main/java/com/fourthFinger/pinkyPlayer/fragments/ViewModelSettings.kt
+++ app/src/main/java/com/fourthFinger/pinkyPlayer/fragments/ViewModelSettings.kt
@@ -1,27 +1,33 @@
 package com.fourthFinger.pinkyPlayer.fragments
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.fourthFinger.pinkyPlayer.R
+import com.fourthFinger.pinkyPlayer.ToastUtil
+import com.fourthFinger.pinkyPlayer.media_controller.SaveFile
 import com.fourthFinger.pinkyPlayer.random_playlist.Settings
 import com.fourthFinger.pinkyPlayer.random_playlist.SettingsRepo
 import kotlin.math.roundToInt
 
-class ViewModelSettings: ViewModel() {
+class ViewModelSettings : ViewModel() {
 
     private val settingsRepo = SettingsRepo.getInstance()
 
     fun getNSongs(): String {
-        return (1.0/settingsRepo.getMaxPercent()).roundToInt().toString()
+        return (1.0 / settingsRepo.getMaxPercent()).roundToInt().toString()
     }
 
     fun getPercentChangeUp(): String {
-        return (100.0/settingsRepo.getPercentChangeUp()).roundToInt().toString()
+        return (100.0 / settingsRepo.getPercentChangeUp()).roundToInt().toString()
     }
 
     fun getPercentChangeDown(): String {
-        return (100.0/settingsRepo.getPercentChangeDown()).roundToInt().toString()
+        return (100.0 / settingsRepo.getPercentChangeDown()).roundToInt().toString()
     }
 
-    fun setSettings(
+    private fun setSettings(
+        context: Context,
         nSongs: Int,
         percentChangeUp: Int,
         percentChangeDown: Int
@@ -35,7 +41,55 @@ class ViewModelSettings: ViewModel() {
                 0.5
             )
         )
+        SaveFile.saveFile(context)
+    }
 
+    fun validateInput(
+        context: Context,
+        nSongs: Int,
+        percentChangeUp: Int,
+        percentChangeDown: Int
+    ): Boolean {
+        return if (nSongs < 1) {
+            ToastUtil.showToast(context, R.string.max_percent_error)
+            false
+        } else if (percentChangeUp < 1 || percentChangeUp > 100) {
+            ToastUtil.showToast(context, R.string.percent_change_error)
+            false
+        } else if (percentChangeDown < 1 || percentChangeDown > 100) {
+            ToastUtil.showToast(context, R.string.percent_change_error)
+            false
+        } else {
+            true
+        }
+    }
+
+    fun fabClicked(
+        context: Context,
+        navController: NavController,
+        nSongs: Int,
+        percentChangeUp: Int,
+        percentChangeDown: Int
+    ) {
+        if (validateInput(context, nSongs, percentChangeUp, percentChangeDown)) {
+            updateSettings(context, nSongs, percentChangeUp, percentChangeDown)
+            if (navController.currentDestination?.id == R.id.FragmentSettings) {
+                navController.popBackStack()
+            } else {
+                return
+            }
+        } else {
+            return
+        }
+    }
+
+    private fun updateSettings(
+        context: Context,
+        nSongs: Int,
+        percentChangeUp: Int,
+        percentChangeDown: Int
+    ) {
+        setSettings(context, nSongs, percentChangeDown, percentChangeUp)
     }
 
 }
